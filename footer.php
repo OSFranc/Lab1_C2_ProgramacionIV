fb9b5636453da3074b37c820a8a32b57ef491644
OSFranc <renecastagnier@gmail.com>
2025-03-18 16:11:17 -0600

    Corrected files and simplified css

--- footer.php
+++ footer.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width= <div id="footerUGB" onclick="Test()"> <ul> <li>Oscar Rene Palacios Franco</li> <li>Cristian Ernesto Valle Flores</li> <li>Examen Parcial - Primer Computo</li> </ul> </div>, initial-scale=1.0">
+    <meta name="viewport" content="width=initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-<div id="footerUGB" onclick="Test()"> <ul> <li>Oscar Rene Palacios Franco</li> <li>Cristian Ernesto Valle Flores</li> <li>Examen Parcial - Primer Computo</li> </ul> </div>
+<div id="footerUGB" onclick="Test()"> <ul> <li>Oscar Rene Palacios Franco</li> <li>Cristian Ernesto Valle Flores</li> <li>Laboratorio 1 - Segundo Computo</li> </ul> </div>
 </body>
 </html>
